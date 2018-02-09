b08d5437ea926e9dbba0e715029106b75e856778
Valentinas Bartusevičius <v.bartusevicius@paysera.com>
2018-02-09 22:39:10 +0200

    removed missing import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/scss/bootstrap.scss';
 import 'toastr/build/toastr.css';
 
-import './main/user-status';
-
 import './main/leaderboard';
 import './main/challenge';
 
